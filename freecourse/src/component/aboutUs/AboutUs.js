3755a579d0281c0cc44e054df2d5e6c1ab998e59
tronngo <tron.ngo23@student.passerellesnumeriques.org>
2022-07-19 08:57:05 +0700

    update course 19/7/2022

--- freecourse/src/component/aboutUs/AboutUs.js
+++ freecourse/src/component/aboutUs/AboutUs.js
@@ -2,7 +2,7 @@ function AboutUs() {
   return (
     <div className="preloader-visible" data-barba="wrapper">
       <div className="barba-container" data-barba="container">
-        <main className="main-content  ">
+        <main className="main-content">
           <div className="content-wrapper  js-content-wrapper">
             <section data-anim="fade" className="breadcrumbs ">
               <div className="container">
@@ -139,7 +139,6 @@ function AboutUs() {
                         </div>
                         <h4 class="stepCard__title">02. Graduate</h4>
                         <p class="stepCard__text">
-                          {" "}
                           Lorem ipsum dolor sit amet, consectetur dolorili
                           adipiscing elit. Felis donec massa aliqua.
                         </p>
@@ -168,6 +167,257 @@ function AboutUs() {
                 </div>
               </div>
             </section>
+            <div class="tabs__content pt-40 js-tabs-content">
+              <div class="tabs__pane -tab-item-1 is-active container">
+                <div
+                  data-anim="slide-up delay-4"
+                  class="row y-gap-30 pt-30 is-in-view"
+                >
+                  <div class="col-lg-4 col-md-6">
+                    <div class="eventCard -type-1">
+                      <div class="eventCard__img">
+                        <img src="img/courses-list/1.png" alt="image" />
+                      </div>
+
+                      <div class="eventCard__bg bg-white">
+                        <div class="eventCard__content y-gap-10">
+                          <div class="eventCard__inner">
+                            <h4 class="eventCard__title text-17 fw-500">
+                              Summer School 2022
+                            </h4>
+                            <div class="d-flex x-gap-15 pt-10">
+                              <div class="d-flex items-center">
+                                <div class="icon-calendar-2 text-16 mr-8"></div>
+                                <div class="text-14">6 April, 2022</div>
+                              </div>
+                              <div class="d-flex items-center">
+                                <div class="icon-location text-16 mr-8"></div>
+                                <div class="text-14">London, UK</div>
+                              </div>
+                            </div>
+                          </div>
+
+                          <div class="eventCard__button">
+                            <a
+                              href="#"
+                              class="button -sm -rounded -outline-purple-1 text-purple-1 px-25"
+                            >
+                              Buy
+                            </a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="col-lg-4 col-md-6">
+                    <div class="eventCard -type-1">
+                      <div class="eventCard__img">
+                        <img src="img/courses-list/3.png" alt="image" />
+                      </div>
+
+                      <div class="eventCard__bg bg-white">
+                        <div class="eventCard__content y-gap-10">
+                          <div class="eventCard__inner">
+                            <h4 class="eventCard__title text-17 fw-500">
+                              Summer School 2022
+                            </h4>
+                            <div class="d-flex x-gap-15 pt-10">
+                              <div class="d-flex items-center">
+                                <div class="icon-calendar-2 text-16 mr-8"></div>
+                                <div class="text-14">6 April, 2022</div>
+                              </div>
+                              <div class="d-flex items-center">
+                                <div class="icon-location text-16 mr-8"></div>
+                                <div class="text-14">London, UK</div>
+                              </div>
+                            </div>
+                          </div>
+
+                          <div class="eventCard__button">
+                            <a
+                              href="#"
+                              class="button -sm -rounded -outline-purple-1 text-purple-1 px-25"
+                            >
+                              Buy
+                            </a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="col-lg-4 col-md-6">
+                    <div class="eventCard -type-1">
+                      <div class="eventCard__img">
+                        <img src="img/courses-list/4.png" alt="image" />
+                      </div>
+
+                      <div class="eventCard__bg bg-white">
+                        <div class="eventCard__content y-gap-10">
+                          <div class="eventCard__inner">
+                            <h4 class="eventCard__title text-17 fw-500">
+                              Summer School 2022
+                            </h4>
+                            <div class="d-flex x-gap-15 pt-10">
+                              <div class="d-flex items-center">
+                                <div class="icon-calendar-2 text-16 mr-8"></div>
+                                <div class="text-14">6 April, 2022</div>
+                              </div>
+                              <div class="d-flex items-center">
+                                <div class="icon-location text-16 mr-8"></div>
+                                <div class="text-14">London, UK</div>
+                              </div>
+                            </div>
+                          </div>
+
+                          <div class="eventCard__button">
+                            <a
+                              href="#"
+                              class="button -sm -rounded -outline-purple-1 text-purple-1 px-25"
+                            >
+                              Buy
+                            </a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="col-lg-4 col-md-6">
+                    <div class="eventCard -type-1">
+                      <div class="eventCard__img">
+                        <img src="img/courses-list/5.png" alt="image" />
+                      </div>
+
+                      <div class="eventCard__bg bg-white">
+                        <div class="eventCard__content y-gap-10">
+                          <div class="eventCard__inner">
+                            <h4 class="eventCard__title text-17 fw-500">
+                              Summer School 2022
+                            </h4>
+                            <div class="d-flex x-gap-15 pt-10">
+                              <div class="d-flex items-center">
+                                <div class="icon-calendar-2 text-16 mr-8"></div>
+                                <div class="text-14">6 April, 2022</div>
+                              </div>
+                              <div class="d-flex items-center">
+                                <div class="icon-location text-16 mr-8"></div>
+                                <div class="text-14">London, UK</div>
+                              </div>
+                            </div>
+                          </div>
+
+                          <div class="eventCard__button">
+                            <a
+                              href="#"
+                              class="button -sm -rounded -outline-purple-1 text-purple-1 px-25"
+                            >
+                              Buy
+                            </a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-lg-4 col-md-6">
+                    <div class="eventCard -type-1">
+                      <div class="eventCard__img">
+                        <img src="img/courses-list/5.png" alt="image" />
+                      </div>
+
+                      <div class="eventCard__bg bg-white">
+                        <div class="eventCard__content y-gap-10">
+                          <div class="eventCard__inner">
+                            <h4 class="eventCard__title text-17 fw-500">
+                              Summer School 2022
+                            </h4>
+                            <div class="d-flex x-gap-15 pt-10">
+                              <div class="d-flex items-center">
+                                <div class="icon-calendar-2 text-16 mr-8"></div>
+                                <div class="text-14">6 April, 2022</div>
+                              </div>
+                              <div class="d-flex items-center">
+                                <div class="icon-location text-16 mr-8"></div>
+                                <div class="text-14">London, UK</div>
+                              </div>
+                            </div>
+                          </div>
+
+                          <div class="eventCard__button">
+                            <a
+                              href="#"
+                              class="button -sm -rounded -outline-purple-1 text-purple-1 px-25"
+                            >
+                              Buy
+                            </a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-lg-4 col-md-6">
+                    <div class="eventCard -type-1">
+                      <div class="eventCard__img">
+                        <img src="img/courses-list/5.png" alt="image" />
+                      </div>
+
+                      <div class="eventCard__bg bg-white">
+                        <div class="eventCard__content y-gap-10">
+                          <div class="eventCard__inner">
+                            <h4 class="eventCard__title text-17 fw-500">
+                              Summer School 2022
+                            </h4>
+                            <div class="d-flex x-gap-15 pt-10">
+                              <div class="d-flex items-center">
+                                <div class="icon-calendar-2 text-16 mr-8"></div>
+                                <div class="text-14">6 April, 2022</div>
+                              </div>
+                              <div class="d-flex items-center">
+                                <div class="icon-location text-16 mr-8"></div>
+                                <div class="text-14">London, UK</div>
+                              </div>
+                            </div>
+                          </div>
+
+                          <div class="eventCard__button">
+                            <a
+                              href="#"
+                              class="button -sm -rounded -outline-purple-1 text-purple-1 px-25"
+                            >
+                              Buy
+                            </a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row justify-center pt-60 lg:pt-40">
+                    <div class="col-auto">
+                      <div class="pagination -buttons">
+                        <button class="pagination__button -prev">
+                          <i class="icon icon-chevron-left"></i>
+                        </button>
+
+                        <div class="pagination__count">
+                          <a href="#">1</a>
+                          <a class="-count-is-active" href="#">
+                            2
+                          </a>
+                          <a href="#">3</a>
+                          <span>...</span>
+                          <a href="#">67</a>
+                        </div>
+
+                        <button class="pagination__button -next">
+                          <i class="icon icon-chevron-right"></i>
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </main>
       </div>
